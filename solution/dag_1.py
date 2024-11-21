427c5978c8cee0986fed6f66eaec197dee597d71
sat4h <146749026+sat4h@users.noreply.github.com>
2024-11-21 17:35:07 +0400

    Add files via upload

---
+++ solution/dag_1.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.sensors.filesystem import FileSensor
+from docker.types import Mount  # Импортируем Mount
+
+default_args = {
+    'owner': 'sat4h',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
+dag = DAG(
+    'audio_to_pdf',
+    default_args=default_args,
+    description='DAG for extracting audio, transforming to text, summarizing, and saving as PDF',
+    schedule_interval=None,
+)
+
+wait_for_new_file = FileSensor(
+    task_id='wait_for_new_file',
+    poke_interval=10,  # Interval to check for new files (in seconds)
+    filepath= '/opt/airflow/data',
+    fs_conn_id='docker_default', # Target folder to monitor
+    dag=dag,
+)
+
+extract_audio = DockerOperator(
+    task_id='extract_audio',
+    image='jrottenberg/ffmpeg',
+    command='-i /data/input_video.mp4 -vn -acodec copy /data/audio.aac',
+    mounts=[Mount(source='/data', target='/data', type='bind')],
+    docker_url="tcp://docker-proxy:2375",
+    auto_remove = True,
+    dag=dag,
+)
+
+transform_audio_to_text = DockerOperator(
+    task_id='transform_audio_to_text',
+    image='sat4h/tf_container2',
+    command='python /data/audio_to_text.py /data/audio.aac /data/text.txt', 
+    mounts=[Mount(source='/data', target='/data', type='bind')],
+    docker_url="tcp://docker-proxy:2375",
+    dag=dag,
+)
+
+
+summarize_text = DockerOperator(
+    task_id='summarize_text',
+    image='sat4h/tf_container2',
+    command='python /data/summarize_text.py /data/text.txt /data/summary.txt',
+    mounts=[Mount(source='/data', target='/data', type='bind')],
+    docker_url="tcp://docker-proxy:2375",
+    dag=dag,
+)
+
+save_to_pdf = DockerOperator(
+    task_id='save_to_pdf',
+    image='sat4h/tf_container2',
+    command= 'python /data/save_to_pdf.py /data/summary.txt /data/result.pdf',
+    mounts=[Mount(source='/data', target='/data', type='bind')],
+    docker_url="tcp://docker-proxy:2375",
+    dag=dag,
+)
+
+wait_for_new_file >> extract_audio >> transform_audio_to_text >> summarize_text >> save_to_pdf
